91ec1e39076ed749f21eba6e85ce556c2c73841b
daramola oluwatoyin <victoria.mnpdm@gmail.com>
2024-07-11 14:24:01 +0100

    mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,4 @@
---setting up mysql database for the airbnb project
-
+-- setting up mysql database for the airbnb project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
